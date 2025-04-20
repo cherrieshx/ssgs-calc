4e3098601c701ae9155759c30105e8799e918561
alehx <h.alessia888@gmail.com>
2025-04-20 18:52:20 +0200

    Inizializzazione

---
+++ calc.test.js
@@ -0,0 +1,9 @@
+const add =  require('./calc').add;
+
+test ('adds 2+2 to equal 4', () => {
+    expect (add (2, 2)).toBe (4);
+});
+
+test ('adds 3+5 to equal 7(fail)', () => {
+    expect (add (5, 3)).toBe (7);
+});
