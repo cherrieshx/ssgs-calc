870f63a9582d6e5a739927d2cae531229092d9ae
alehx <h.alessia888@gmail.com>
2025-04-21 15:07:46 +0200

    Implementazione dei test con covergae al 100%

--- calc.test.js
+++ calc.test.js
@@ -1,9 +1,37 @@
-const add =  require('./pages/calc').add;
+const { add, sub, mul, div, pow} =  require('./pages/calc');
+
+test('if one or both input are not numbers, throw error', () => {
+    expect(() => add('a', 2)).toThrow('ERROR:Invalid input! Please enter valid numbers.');
+    expect(() => sub(2, 'a')).toThrow('ERROR:Invalid input! Please enter valid numbers.');
+    expect(() => mul('a', 'b')).toThrow('ERROR:Invalid input! Please enter valid numbers.');
+    expect(() => div(2, 'a')).toThrow('ERROR:Invalid input! Please enter valid numbers.');
+    expect(() => pow('a', 2)).toThrow('ERROR:Invalid input! Please enter valid numbers.');
+});
 
 test ('adds 2+2 to equal 4', () => {
     expect (add (2, 2)).toBe (4);
 });
 
-test ('adds 3+5 to equal 7(fail)', () => {
-    expect (add (5, 3)).toBe (7);
+
+test ('subtracts 5-3 to equal 2', () => {
+    expect (sub (5, 3)).toBe (2);
 });
+
+test ('multiplies 3*3 to equal 9', () => {
+    expect (mul (4, 3)).toBe (12);
+});
+
+test ('divides 6/3 to equal 2', () => {
+    expect (div (6, 3)).toBe (2);
+});
+
+test ('divides 6/0 to throw error', () => {
+    expect ( () => { div (6, 0) } ).toThrow ("ERROR: Division by zero is not allowed!");
+});
+
+test ('3 power 3 to equal 27', () => {
+    expect (pow (3, 3)).toBe (27);
+});
+
+
+
--- pages/calc.js
+++ pages/calc.js
@@ -1,20 +1,26 @@
-const readline = require('readline/promises');
-const { stdin: input, stdout: output } = require('node:process');
-
+function control(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+        throw new Error('ERROR:Invalid input! Please enter valid numbers.');
+    }
+}
 
 function add(a, b) {
+    control(a,b);
     return a + b;
 }
 
 function sub(a, b) {
+    control(a,b);
     return a - b;
 }
 
 function mul(a, b) {
+    control(a,b);
     return a * b;
 }
 
 function div(a, b) {
+    control(a,b);
     if (b === 0) {
         throw new Error("ERROR: Division by zero is not allowed!");
     }
@@ -22,89 +28,10 @@ function div(a, b) {
 }
 
 function pow(a, b) {
+    control(a,b);
     return Math.pow(a, b);
 }
 
-async function calc() {
-    console.log("Welcome to SSGS Calculator!");
-    const rl = readline.createInterface({ input, output });
-
-    while (true) {
-        console.log("Please choose an operation with the given numbers or enter 0 to exit:");
-        console.log("1. Addition");
-        console.log("2. Subtraction");
-        console.log("3. Multiplication");
-        console.log("4. Division");
-        console.log("5. Power (Exponentiation)");
-
-        try {
-            let operation = parseFloat(await rl.question("Enter your choice: "));
-
-            if (
-                isNaN(operation) ||
-                !Number.isInteger(operation) ||
-                operation < 0 ||
-                operation > 5
-            ) {
-                console.log("Invalid option! Please enter a valid option.");
-                continue;
-            }
-
-            if (operation === 0) {
-                console.log("Exiting the calculator. Goodbye!");
-                rl.close();
-                process.exit(0);
-            }
-
-            const n1 = parseFloat(await rl.question("Enter the first number: "));
-            const n2 = parseFloat(await rl.question("Enter the second number: "));
-
-            if (isNaN(n1) || isNaN(n2)) {
-                console.log("Invalid input! Please enter valid numbers.");
-                continue;
-            }
-
-            let result;
-
-            switch (operation) {
-                case 1:
-                    result = add(n1, n2);
-                    console.log(`The sum of ${n1} and ${n2} is ${result}`);
-                    break;
-                case 2:
-                    result = sub(n1, n2);
-                    console.log(`The difference of ${n1} and ${n2} is ${result}`);
-                    break;
-                case 3:
-                    result = mul(n1, n2);
-                    console.log(`The product of ${n1} and ${n2} is ${result}`);
-                    break;
-                case 4:
-                    try {
-                        result = div(n1, n2);
-                        console.log(`The quotient of ${n1} and ${n2} is ${result}`);
-                    } catch (error) {
-                        console.log(error.message);
-                    }
-                    break;
-                case 5:
-                    result = pow(n1, n2);
-                    console.log(`The result of ${n1} raised to the power of ${n2} is ${result}`);
-                    break;
-            }
-
-        
-        } catch (error) {
-            console.error("Unexpected error:", error);
-            rl.close();
-            process.exit(1);
-        }
-    }
-}
-
-calc();
-
-
 module.exports = {
-    add
+    control,add,sub,mul,div,pow
 };
